99a8e8d6b21c17a5dc01be80f2215dcf4df99472
Mati Joseph <crucialniccur@gmail.com>
2025-04-12 15:09:27 +0300

    add an onChange to the input in the Keypad component that logs a message when entering input

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,13 @@
 // Code Keypad Component Here
 import React from "react";
 function Keypad() {
+  function handleChange(event) {
+    // console.log(event.target.value)
+    console.log("Entering password...");
+  }
   return (
     <div>
-      <input type="password" />
+      <input type="password" onChange={handleChange} />
     </div>
   );
 }
